c1678c06f62af1fdfd3d64073e175d3b4f265625
adeyemimike123 <ayobami732000@gmail.com>
2024-04-26 05:34:43 +0100

    initial commit

---
+++ target/debug/deps/hello_rust-42d810296824bcdd.d
@@ -0,0 +1,5 @@
+/Users/bishopakandeayobami/hello-rust/target/debug/deps/hello_rust-42d810296824bcdd: src/main.rs
+
+/Users/bishopakandeayobami/hello-rust/target/debug/deps/hello_rust-42d810296824bcdd.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/bishopakandeayobami/hello-rust/target/debug/hello-rust: /Users/bishopakandeayobami/hello-rust/src/main.rs
