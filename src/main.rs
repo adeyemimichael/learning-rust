44d72dba575075ef0839b765f4f6f7497757f100
adeyemimike123 <ayobami732000@gmail.com>
2024-04-29 08:39:30 +0100

    commited another change

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let max_value = 300;
+    let mut even_numbers: Vec<i32> = Vec::new();
+
+    for i in (1..=max_value).rev() {
+        if i % 2 == 0 {
+            even_numbers.push(i);
+        }
+    }
+
+    for even_number in &even_numbers {
+        println!("{}", even_number); // Print even numbers in descending order
+    }
+
+    println!("Even numbers sequence: {:?}", even_numbers);
 }
